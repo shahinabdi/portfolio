1049e8021b486efed28129cf0e20ce35d398d574
Shahin ABDI <contact@shahinabdi.fr>
2024-10-24 08:31:39 +0200

    fix: update Vite configuration to resolve MIME type issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { createHtmlPlugin } from 'vite-plugin-html'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/shahinabdi.github.io/' : '/',
+  base: '/shahinabdi.github.io/',
   plugins: [
     vue(),
     createHtmlPlugin({
